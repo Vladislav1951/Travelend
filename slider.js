1b200d48ab740c6d4a68c5ffa92c3609b12b6828
Vladislav1951 <ivanovvlad1951@mail.ru>
2024-07-29 00:14:38 +0300

    Initial commit

---
+++ slider.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+    slidesPerView: 'auto',
+    freeMode: true,
+    freeModeSticky: true,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true
+  });
+  
